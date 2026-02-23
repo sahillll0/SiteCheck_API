f7455eb798c617eb7cf83aa05d5b770a4a99c0ea
sahillll0 <sahiltippe143@gmail.com>
2026-02-23 15:24:45 +0530

    feat: expand CORS allowed origins to include a new API endpoint and an empty string.

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ app.use(cors({
         const allowedOrigins = [
             "http://localhost:5173",
             "https://site-check-com.vercel.app",
+            "https://site-check-api.vercel.app/api/auth/check",
+            ""
             process.env.FRONTEND_URL
         ].filter(Boolean);
         if (allowedOrigins.includes(origin) || origin.endsWith(".vercel.app")) {
